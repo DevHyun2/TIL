ac0b354e940073222a72d7468ae368d94009a935
DaeHyun2 <aaz11512@gmail.com>
2024-03-12 23:01:21 +0900

    day01

---
+++ day01/Hello.java
@@ -0,0 +1,14 @@
+package com.shinhan.day01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요");
+		System.out.println("안녕하세요");
+		System.out.println("안녕하세요");
+		System.out.println("안녕하세요");
+		System.out.println("안녕하세요");
+	}
+
+}
